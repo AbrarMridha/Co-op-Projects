de43ae55f69763e3e568d4b2bf4e92d70e17f290
AbrarMridha <154542170+AbrarMridha@users.noreply.github.com>
2024-10-08 22:02:18 -0230

    Update main.py

--- Image Processing/main.py
+++ Image Processing/main.py
@@ -1,5 +1,3 @@
-# Name - Abrarul Hoque Mridha
-# Student ID - 202141040
 # This program loads an image and compute the intensity histogram for each color channel of the given input image.
 # It also performs histogram stretching, aggressive histogram stretching and histogram equalization.
 # This is the main file that performs the run method in the class ImageProcessingApp in GUI.py
